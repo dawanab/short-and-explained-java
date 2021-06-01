09f81c97805d6336463815d3be2ac7e1320df00d
Dawana Baker <66096734+dawanab@users.noreply.github.com>
2021-05-31 20:34:48 -0500

    Added Multiply Two Floating Point Numbers Program

---
+++ MultiplyTwoFloatingPointNumbers.java
@@ -0,0 +1,17 @@
+package Introduction;
+
+public class MultiplyTwoFloatingPointNumbers {
+    
+    /* This program will multiply two floating point numbers, store the
+    result and display it on screen
+     */
+
+     public static void main(String[] args) {
+         float firstNum = 1.5f;
+         float secondNum = 2.0f;
+
+         float product = firstNum * secondNum;
+
+         System.out.println(product);
+     }
+}
