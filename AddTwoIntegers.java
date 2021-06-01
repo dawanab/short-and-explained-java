398ae052a0835833acddb2fb44cd053387cf9dab
Dawana Baker <66096734+dawanab@users.noreply.github.com>
2021-05-31 20:33:40 -0500

    Added Add Two Integers Program

---
+++ AddTwoIntegers.java
@@ -0,0 +1,21 @@
+package Introduction;
+import java.util.Scanner;
+
+public class AddTwoIntegers {
+    
+    /* This program will store and add two integer numbers. After the addition, 
+    the final sum is displayed on the screen. 
+    */
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter two numbers: ");
+        int x  = scan.nextInt();
+        int y = scan.nextInt();
+        scan.close();
+
+        // add two numbers
+        System.out.println("The sum of " + x + " + " + y + " = " + (x + y));
+    }
+}
